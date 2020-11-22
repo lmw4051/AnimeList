818e23af978a0582c43c10b76dd843f1cfdd71b4
David <dvdlee41@gmail.com>
2020-11-22 13:16:14 +0800

    Initial Commit

---
+++ AnimteMangaTopItemList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimteMangaTopItemList
+//
+//  Created by David on 2020/11/22.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
