c3c10c55564413b36b57a8c1080743665998f805
David <dvdlee41@gmail.com>
2020-11-24 01:51:28 +0800

    Adding deleteButton for FavoriteItemCell

--- AnimteMangaTopItemList/Views/CustomTableViewCell/FavoriteItemCell.swift
+++ AnimteMangaTopItemList/Views/CustomTableViewCell/FavoriteItemCell.swift
@@ -15,5 +15,32 @@ class FavoriteItemCell: ItemCell {
     didSet {
       updateUI(item)
     }
-  }    
+  }
+  
+  lazy var deleteButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setImage(UIImage(named: "delete"), for: .normal)
+    button.addTarget(self, action: #selector(handleDeletion), for: .touchUpInside)
+    return button
+  }()
+  
+  @objc private func handleDeletion() {
+    guard let item = self.item else { return }
+    
+    do {
+      let realm = try Realm()
+      try realm.write {
+        realm.delete(item)
+      }
+    } catch let error as NSError {
+      showError(error: error)
+    }
+  }
+  
+  override func configureViews() {
+    super.configureViews()
+    addSubview(deleteButton)
+    deleteButton.anchor(top: nil, leading: nil, bottom: nil, trailing: trailingAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6), size: .init(width: 32, height: 32))
+    deleteButton.centerYInSuperview()
+  }
 }
