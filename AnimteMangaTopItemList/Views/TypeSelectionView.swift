9dcdcad874c5d55518923de197feed2cd3aaa8d2
David <dvdlee41@gmail.com>
2020-11-24 02:55:16 +0800

    . Adding UITextFieldDelegate methods

--- AnimteMangaTopItemList/Views/TypeSelectionView.swift
+++ AnimteMangaTopItemList/Views/TypeSelectionView.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class TypeSelectionView: UIView {
   // MARK: - Instance Properties
   let typeTF = CustomTextField(padding: 12, cornerRadius: 6, backgroundColor: .init(white: 1, alpha: 0.3), textColor: .darkGray)
@@ -17,6 +19,10 @@ class TypeSelectionView: UIView {
   override init(frame: CGRect) {
     super.init(frame: frame)
     backgroundColor = .clear
+    
+    typeTF.delegate = self
+    subTypeTF.delegate = self
+    
     let inputStackView = vStack(
       hStack(createTypeTitleLabel(title: "Type"), typeTF, spacing: 8),
       hStack(createTypeTitleLabel(title: "Subtype"), subTypeTF, spacing: 8), spacing: 8, distribution: .fillEqually)
@@ -27,11 +33,34 @@ class TypeSelectionView: UIView {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
   // MARK: - Helper Methods
   private func createTypeTitleLabel(title: String) -> UILabel {
     let typeLabel = UILabel()
     typeLabel.text = title
     typeLabel.constrainWidth(constant: 80)
     return typeLabel
+  }
+}
+
+// MARK: - UITextFieldDelegate Methods
+extension TypeSelectionView: UITextFieldDelegate {
+  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    if textField == typeTF {
+      typeTF.resignFirstResponder()
+      presentTypePicker()
+    } else {
+      subTypeTF.resignFirstResponder()
+      presentSubtypePicker()
+    }
+    return false
+  }
+  
+  private func presentTypePicker() {
+    print("presentTypePicker")
+  }
+  
+  private func presentSubtypePicker() {
+    print("presentSubtypePicker")
   }
 }
